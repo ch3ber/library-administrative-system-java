dca0c6a79c07b62e343c40ba6615f8ab84b0aa05
Eber Alejo <eber4202@hotmail.com>
2024-10-31 19:43:21 -0600

    REFACTOR: finds in inventary controller

--- src/controllers/InventoryController.java
+++ src/controllers/InventoryController.java
@@ -16,14 +16,23 @@ import javax.swing.*;
  */
 public class InventoryController {
   LibraryModel library;
-  public InventoryController (LibraryModel library) {
+  BookInfoView bookInfoView = new BookInfoView();
+
+  public InventoryController(LibraryModel library) {
     this.library = library;
+  }
+
+  private void hanldeFind(String queryId, BookSearchType bookSearchType) {
+    GenericBookModel foundBook = library.findBook(queryId, bookSearchType);
+    if (foundBook == null) {
+      JOptionPane.showMessageDialog(null, "Libro no encontrado.");
+      return;
+    }
+    bookInfoView.showAllInfo(foundBook.getAllInfo());
   }
 
   public void evalOption(Object selectedOption) {
     String parsedSelectedOption = "";
-
-    BookInfoView bookInfoView = new BookInfoView();
 
     while (!parsedSelectedOption.equals("Atrás")) {
       // the user select's cancel button
@@ -32,46 +41,36 @@ public class InventoryController {
       // if the user no cancel, parse the option from selected option
       parsedSelectedOption = selectedOption.toString();
 
+      // var for find book
       String queryId;
-      GenericBookModel foundBook;
 
+      // vars for add book
       String title;
       String author;
       String category;
       String numCopies;
       int numCopiesParsed;
       String id;
+
       switch (parsedSelectedOption) {
         case "Buscar por ID":
           queryId = JOptionPane.showInputDialog(null, "Ingresa su ISBN o ISSN", "");
-          foundBook = library.findBook(queryId, BookSearchType.ID);
-          if (foundBook == null) {
-            JOptionPane.showMessageDialog(null, "Libro no encontrado.");
-            parsedSelectedOption = "Atrás";
-            break;
-          }
-          bookInfoView.showAllInfo(foundBook.getAllInfo());
-          System.out.println(foundBook.getAllInfo());
+          this.hanldeFind(queryId, BookSearchType.ID);
           parsedSelectedOption = "Atrás";
           break;
+
         case "Buscar por titulo":
-          queryId = JOptionPane.showInputDialog(null, "Ingresa su ISBN o ISSN", "");
-          foundBook = library.findBook(queryId, BookSearchType.TITLE);
-          if (foundBook != null) {
-            System.out.println(foundBook.getAllInfo());
-            bookInfoView.showAllInfo(foundBook.getAllInfo());
-          }
+          queryId = JOptionPane.showInputDialog(null, "Ingresa su titulo", "");
+          this.hanldeFind(queryId, BookSearchType.TITLE);
           parsedSelectedOption = "Atrás";
           break;
+
         case "Buscar por autor":
-          queryId = JOptionPane.showInputDialog(null, "Ingresa su ISBN o ISSN", "");
-          foundBook = library.findBook(queryId, BookSearchType.AUTHOR);
-          if (foundBook != null) {
-            System.out.println(foundBook.getAllInfo());
-            bookInfoView.showAllInfo(foundBook.getAllInfo());
-          }
+          queryId = JOptionPane.showInputDialog(null, "Ingresa el nombre del autor", "");
+          this.hanldeFind(queryId, BookSearchType.AUTHOR);
           parsedSelectedOption = "Atrás";
           break;
+
         case "Agregar un nuevo libro":
           title = JOptionPane.showInputDialog(null, "Ingresa su titulo", "");
           author = JOptionPane.showInputDialog(null, "Ingresa su autor", "");
